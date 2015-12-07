fb9907c84679a03dbec89a48150ab1b7469549d7
Greg Dietsche <gdietsche@sajan.com>
2015-12-07 16:34:19 -0600

    add revert script
    Signed-off-by: Greg Dietsche <gdietsche@sajan.com>

---
+++ FixHintPaths-Revert.ps1
@@ -0,0 +1,15 @@
+﻿$tfexe = 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Ide\TF.exe'
+
+ls -Recurse -include *.csproj, *.sln, *.fsproj, *.vbproj |
+  foreach {
+
+    $content = cat $_.FullName | Out-String
+    $origContent = $content
+    $content = $content -replace "<HintPath>\$\(SolutionDir\)packages", "<HintPath>..\packages"
+    if ($origContent -ne $content)
+    {	
+	& $tfexe "checkout" $_.FullName
+        $content | out-file -encoding "UTF8" $_.FullName
+        write-host messed with $_.Name
+    }		    
+}
